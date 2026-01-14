7895faa6c644e74cee7eb343f4e7ceb14d77fa2c
Ambar Bansal <ambarbansal2@gmail.com>
2026-01-15 00:58:50 +0530

    Implement array reversal after swap operation
    Added a reverse function to reverse the array after swapping elements.

--- Swap.java
+++ Swap.java
@@ -6,9 +6,24 @@ public class Swap {
         swap(arr,1,3);
 
         System.out.println(Arrays.toString(arr));
+        rev(arr);
+        System.out.println(Arrays.toString(arr));
 
 
-    }static void swap(int[] arr,int index1,int index2){
+    }
+    static void rev(int[]arr){
+        int start = 0;
+        int end = arr.length-1;
+         while(start<end){
+             swap(arr,start,end);
+             start++;
+             end--;
+
+         }
+    }
+
+
+    static void swap(int[] arr,int index1,int index2){
         int temp = arr[index1];
         arr[index1]=arr[index2];
         arr[index2]=temp;
