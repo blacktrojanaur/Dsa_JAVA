c05bf9ed5513979476fe901b4255f69061b63704
Ambar Bansal <ambarbansal2@gmail.com>
2026-03-11 13:18:56 +0530

    Add files via upload

---
+++ revQueue.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class revQueue {
+
+    public static void reverse(Queue<Integer> queue, int k) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < k; i++) {
+            st.push(queue.remove());
+        }
+
+        for (int i = 0; i < k; i++) {
+            queue.offer(st.pop());
+        }
+
+        int count = queue.size() - k;
+
+        for (int i = 0; i < count; i++) {
+            queue.offer(queue.remove());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+
+        int k = 3;
+
+        reverse(q, k);
+
+        System.out.println(q);
+    }
+}
