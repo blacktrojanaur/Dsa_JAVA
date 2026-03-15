fa44d6969cedbadac5afa948b341b6d618c22f16
Ambar Bansal <ambarbansal2@gmail.com>
2026-03-15 22:56:39 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -11,6 +11,7 @@ public class LinearSearch {
         if (arr.length== 0){
             return -1;
         }
+
         for (int i = 0 ; i< arr.length;i++){
             int element = arr[i];
             if(element==target){
