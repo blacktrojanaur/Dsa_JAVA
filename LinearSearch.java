6e3c97b1a2d5b87d0756f47231300fac55cfd3e6
Ambar Bansal <ambarbansal2@gmail.com>
2026-03-23 20:22:59 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -3,6 +3,7 @@ public class LinearSearch {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+
         int[]arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
         int target=in.nextInt();
         int ans = linear(arr,target);
